1b6dc8fd7d18ab7be0e1b3701f912d8b570ee979
Bryan Tanifum <bryan.tanifum@ed.gov>
2018-09-07 00:09:45 +0000

    Updated network definition to fix validating grantee function

--- deployNetwork.sh
+++ deployNetwork.sh
@@ -1,3 +1,4 @@
-composer network install -a $1@$2.bna -c PeerAdmin@hlfv1
-composer network start --networkName $1 --networkVersion $2 -c PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw
+npm run prepublish
+composer network install -a $1.bna -c PeerAdmin@hlfv1
+composer network start --networkName $1 --networkVersion $2 -c PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw -f admin@$1.card
 
